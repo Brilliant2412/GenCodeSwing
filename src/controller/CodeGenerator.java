1def12935ccacde1013a646b83d4ee55fc50e566
Hung <miaooo.sl.ys.lyn.hyeon.lb@gmail.com>
2021-01-22 16:46:36 +0700

    doc jar nhung chua hoan thien

--- src/controller/CodeGenerator.java
+++ src/controller/CodeGenerator.java
@@ -1,15 +1,27 @@
 package controller;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 import java.util.Vector;
-
 import exceptions.WrongExcelTypeException;
 import model.ColumnProperty;
 import model.TableInfo;
 import model.TableSet;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.jar.Attributes;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
 
 
 public class CodeGenerator {
@@ -18,6 +30,7 @@ public class CodeGenerator {
     static String pathOne;
     static String pathTwo;
     static String pathSubObjs;
+    public static final byte[] BUFFER = new byte[1024];
 
     public CodeGenerator() {
         url="";
@@ -63,5 +76,96 @@ public class CodeGenerator {
         for(int i = 0; i < tableSet.subTables.size(); i++){
             SubObj.genSubObj(tableSet.subTables.get(i), pathTwo,tableSet.tableInfo.tableName);
         }
+
+        String jar_file = pathString+"\\" + tableSet.tableInfo.tableName +".jar";
+        String input_folder = pathString+"\\"+ tableSet.tableInfo.tableName;
+
+        File outputZipfile = new File(jar_file);
+        File inputDir = new File(input_folder);
+        createJarFile(inputDir, outputZipfile);
+//        File current = new File(pathString +"\\"+tableSet.tableInfo.tableName);
+//        deleteFolder(current);
+
+    }
+    public static void createJarFile(File inputDir, File outputJarFile){
+        outputJarFile.getParentFile().mkdirs();
+        String inputDirPath = inputDir.getAbsolutePath();
+
+        // prepare Manifest file
+        String version = "1.0.0";
+        String author = "hung";
+        Manifest manifest = new Manifest();
+        Attributes global = manifest.getMainAttributes();
+        global.put(Attributes.Name.MANIFEST_VERSION, version);
+        global.put(new Attributes.Name("Created-By"), author);
+
+        FileOutputStream fos = null;
+        JarOutputStream jos = null;
+        try {
+
+            List<File> allFiles = listChildFiles(inputDir);
+
+            // Tạo đối tượng JarOutputStream để ghi file jar.
+            fos = new FileOutputStream(outputJarFile);
+            jos = new JarOutputStream(fos, manifest);
+            for (File file : allFiles) {
+                String filePath = file.getAbsolutePath();
+
+                System.out.println("Creating jar: " + filePath);
+                // entryName: is a relative path.
+                String entryName = filePath.substring(inputDirPath.length() + 1);
+
+                // create JarEntry
+                JarEntry je = new JarEntry(entryName);
+                je.setComment("Creating Jar");
+                je.setTime(Calendar.getInstance().getTimeInMillis());
+                // Thêm entry vào file jar.
+                jos.putNextEntry(je);
+
+                // Đọc dữ liệu của file và ghi vào JarOutputStream.
+                InputStream fileIs = new FileInputStream(filePath);
+                int len;
+                while ((len = fileIs.read(BUFFER)) > 0) {
+                    jos.write(BUFFER, 0, len);
+                }
+                fileIs.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            closeStream(jos);
+            closeStream(fos);
+        }
+    }
+    private static void closeStream(OutputStream out) {
+        try {
+            out.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    private static List<File> listChildFiles(File dir) throws IOException {
+        List<File> allFiles = new ArrayList<>();
+
+        File[] childFiles = dir.listFiles();
+        for (File file : childFiles) {
+            if (file.isFile()) {
+                allFiles.add(file);
+            } else {
+                List<File> files = listChildFiles(file);
+                allFiles.addAll(files);
+            }
+        }
+        return allFiles;
+    }
+    private  static void deleteFolder(File file){
+        for (File subFile : file.listFiles()) {
+            if(subFile.isDirectory()) {
+                deleteFolder(subFile);
+            } else {
+                subFile.delete();
+            }
+        }
+        file.delete();
     }
 }
